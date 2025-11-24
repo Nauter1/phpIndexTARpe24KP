af47d5d0e828c6483b8a8bf1416acd2ba4ff9917
Nauter <Kaur.Pakaste@gmail.com>
2025-11-24 12:33:45 +0200

    Jätsin dropdown menu pooleli, ei saa aru kuidas parandada.

--- phpIndex/nav.php
+++ phpIndex/nav.php
@@ -1,28 +1,42 @@
-<nav>
-    <ul>
-        <li>
-            <a href="?leht=kodu.php">Kodu</a>
-        </li>
-        <li>
-            <a href="?leht=jsToo.php">JS joonis</a>
-        </li>
-        <li>
-            <a href="?leht=jsVeebiKalkulaator.php">JS Kalkulaator</a>
-        </li>
-        <li>
-            <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
-        </li>
-        <li>
-            <a href="https://kaurpakaste24.thkit.ee/" target="_blank">Vana koduleht</a>
-        </li>
-        <li>
-            <a href="?leht=gitKasud.php">GIT Käsud</a>
-        </li>
-        <li>
-            <a href="?leht=tekstfunktsioonid.php">Tekstfunktsioonid</a>
-        </li>
-        <li>
-            <a href="?leht=matemaatilised.php">Matem. Funktsioonid</a>
-        </li>
-    </ul>
-</nav>
+
+    <nav class="menu">
+        <ul>
+            <li>
+                <a href="?leht=kodu.php">Kodu</a>
+            </li>
+            <li>
+                <a href="?leht=jsToo.php">JS joonis</a>
+            </li>
+            <li>
+                <a href="?leht=jsVeebiKalkulaator.php">JS Kalkulaator</a>
+            </li>
+            <li>
+                <a href="https://kaurpakaste24.thkit.ee/" target="_blank">Vana koduleht</a>
+            </li>
+            <li>
+                <a href="?leht=gitKasud.php">GIT Käsud</a>
+            </li>
+            <li>
+                <a href="?leht=tekstfunktsioonid.php">Tekstfunktsioonid</a>
+            </li>
+            <li>
+                <a href="?leht=matemaatilised.php">Matem. Funktsioonid</a>
+            </li>
+            <li>
+                <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
+            </li>
+            <li><a href="#">Funktsioonid</a>
+                <ul class="dropdown">
+                    <li>
+                        <a href="?leht=tekstfunktsioonid.php">Tekstfunktsioonid</a>
+                    </li>
+                    <li>
+                        <a href="?leht=matemaatilised.php">Matem. Funktsioonid</a>
+                    </li>
+                    <li>
+                        <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
